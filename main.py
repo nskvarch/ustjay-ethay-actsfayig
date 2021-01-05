0cb6733975944e842a265d6006b89624199a5d2e
Niels Skvarch <nielsk01@noa.nintendo.com>
2021-01-05 12:16:53 -0800

    files for lesson 5 assignment

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import os
-
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 
 def get_fact():
-
-    response = requests.get("http://unkno.com")
+    """Gets a random fact from http://unkno.com/"""
+    response = requests.get("http://unkno.com/")
 
     soup = BeautifulSoup(response.content, "html.parser")
-    facts = soup.find_all("div", id="content")
+    fact = soup.find("div", id="content")
 
-    return facts[0].getText()
+    return fact
 
 
-@app.route('/')
+def pig_fact(fact):
+    """Translates fact to piglatin using a microservice on Heroku"""
+    piglatinize_url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    request = {"input_text": fact}
+    response = requests.post(piglatinize_url, request, allow_redirects=False)
+    link = response.headers["Location"]
+
+    return link
+
+
+@app.route('/', methods=["GET"])
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    link = pig_fact(fact)
+
+    return render_template("base.jinja2", link=link)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
